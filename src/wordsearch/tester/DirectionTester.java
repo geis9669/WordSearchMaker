d2c49dd96935a81cfab20c22cc660a9b5a5b4e31
geis9669 <42647833+geis9669@users.noreply.github.com>
2020-02-24 11:01:56 -0700

    made a tester for the direction tester

---
+++ src/wordsearch/tester/DirectionTester.java
@@ -0,0 +1,38 @@
+package wordsearch.tester;
+
+import wordsearch.model.Direction;
+import wordsearch.model.IntPair;
+
+public class DirectionTester {
+
+
+    public static void main(String[] args)
+    {
+        Direction direction = new Direction();
+
+        String text ="";
+        for(int time = 0; time < 9; time++)
+        {
+            IntPair pair= direction.getRandomDirection();
+            text += "" + pair.getRowDirection()+"  "+ pair.getColDirection()+ "\n";
+        }
+        System.out.println(text);
+
+        direction.reset();
+        direction.set(new IntPair(0,0), true);
+        text = "";
+        for(int time = 0; time < 9; time++)
+        {
+            IntPair pair= direction.getRandomDirection();
+            if(pair != null)
+            {
+                text += "" + pair.getRowDirection()+"  "+ pair.getColDirection()+ "\n";
+            }
+            else
+            {
+                text+= "null\n";
+            }
+        }
+        System.out.println(text);
+    }
+}
