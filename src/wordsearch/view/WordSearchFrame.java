470fa8e06ab08cc8930d6d1f32a207b11d5d3325
geis9669 <42647833+geis9669@users.noreply.github.com>
2020-03-14 21:56:07 -0600

    created a frame to hold everything

---
+++ src/wordsearch/view/WordSearchFrame.java
@@ -0,0 +1,27 @@
+package wordsearch.view;
+
+import wordsearch.controller.WordSearchController;
+import javax.swing.*;
+
+public class WordSearchFrame extends JFrame {
+    private WordSearchController controller;
+    private JPanel panel;
+
+    public WordSearchFrame(WordSearchController controller)
+    {
+        super();
+        this.controller = controller;
+        this.panel = new WordSearchPanel(controller);
+
+        this.add(panel);
+
+        setTitle("Word Search Maker");
+        setSize(800,800);
+        setResizable(false);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setVisible(true);
+    }
+
+}
